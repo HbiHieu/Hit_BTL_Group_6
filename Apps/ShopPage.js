ce9f277692be258937c26dec8f17ba595d4e25d8
HbiHieu <89714916+HkHieus@users.noreply.github.com>
2023-01-14 01:12:16 +0700

    structure app

---
+++ Apps/ShopPage.js
@@ -0,0 +1,5 @@
+import { headerString } from "../Layout/Header.js"
+
+const header =  document.getElementById("header") ;
+
+header.innerHTML = headerString ;
---
+++ Apps/index.js
@@ -0,0 +1,3 @@
+const handleOpenCart = () => {
+    alert('something') ;
+}
